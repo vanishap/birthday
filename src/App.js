bc805cae78abb85b6d6b33c176c93c4369a0dfbb
vanishap <vanishareddyp@gmail.com>
2022-11-04 10:08:46 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {   
+  const [userName, setUserName] = useState('Madhan');
+  const [age, setAge] = useState();
+  const [img, setImg] = useState();
+  const [users, setUsers] = useState(data);
+  const clickHandler =(users)=>{
+  setUsers([]);
+}
+const nameHandler = (e)=>{
+e.target.innerHTML = e.target.innerHTML+'*'
+  
+}
+const length = users.length;
+
+  return ( <>
+    <h3>{length} birthdays today</h3>
+    <h2 onClick={()=>setUserName('Vanisha')}>{userName}</h2>
+    { users.map((user) => { 
+    const {name, age, img} = user;
+    return ( 
+      <main>
+        <section> 
+          <article className='person'>
+            <img src={img}></img>
+            <div>
+              <h4 value={name}  className= 'name'onClick={(e)=>nameHandler(e)}>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        </section>
+      </main>
+    )
+  })}
+      <button className='btn' onClick={() =>clickHandler(users)}>Clear Items</button>  
+  </>
+  );   
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = [
+  {name:"Bertie Yates",
+   age: 29,
+   img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+   id: 1},
+  {name:"Hester Hogan",
+   age: 32,
+   img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+   id: 2},
+   {name:"Larry Little",
+   age: 36,
+   img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+   id: 3},
+   {name:"Sean Walsh",
+   age: 34,
+   img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+   id: 4},
+   {name:"Lola Gardner",
+   age: 29,
+   img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+   id: 5},
+];
+export default data;
